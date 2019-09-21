743dad8fed1194d319bd7df75ca90144c38eb5d6
BerkiAttila <attila.berki3@gmail.com>
2019-09-21 13:09:38 +0200

    Updating geofence notifier to support android version >8.0

--- src/android/GeoNotificationNotifier.java
+++ src/android/GeoNotificationNotifier.java
@@ -1,6 +1,7 @@
 package com.cowbell.cordova.geofence;
 
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,9 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+import android.os.Build;
+import android.graphics.Color;
+import android.annotation.SuppressLint;
 
 public class GeoNotificationNotifier {
     private NotificationManager notificationManager;
@@ -22,17 +26,32 @@ public class GeoNotificationNotifier {
         this.context = context;
         this.beepHelper = new BeepHelper();
         this.logger = Logger.getLogger();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            @SuppressLint("WrongConstant") NotificationChannel notificationChannel = new NotificationChannel("geofence-notification", "Crew IADP Notifications", NotificationManager.IMPORTANCE_HIGH);
+             // Configure the notification channel.
+             notificationChannel.setDescription("Crew notifications");
+             notificationChannel.enableLights(true);
+             notificationChannel.setLightColor(Color.RED);
+             notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+             notificationChannel.enableVibration(true);
+             notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 
     public void notify(Notification notification) {
         notification.setContext(context);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+
+        String NOTIFICATION_CHANNEL_ID = "geofence-notification";
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
             .setVibrate(notification.getVibrate())
             .setSmallIcon(notification.getSmallIcon())
             .setLargeIcon(notification.getLargeIcon())
             .setAutoCancel(true)
             .setContentTitle(notification.getTitle())
-            .setContentText(notification.getText());
+            .setContentText(notification.getText())
+            .setPriority(NotificationCompat.PRIORITY_MAX);
 
         if (notification.openAppOnClick) {
             String packageName = context.getPackageName();
